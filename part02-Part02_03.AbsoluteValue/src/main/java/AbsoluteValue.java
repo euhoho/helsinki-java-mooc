858ac54a4ff5346a0baf2b4c29d39cc7df441479
euhoho <yosoydarky@gmail.com>
2026-05-05 10:52:02 +0200

    Part 2 exercise 3 - Absolute Value

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -6,5 +6,14 @@ public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number = Integer.parseInt(scanner.nextLine());
+
+        if (number<0) {
+            int mult = number*-1;
+            System.out.println(mult);
+        } else {
+            System.out.println(number);
+        }
+        scanner.close();
     }
 }
