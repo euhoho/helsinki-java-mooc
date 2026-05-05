4319b76124fa33f380264b205268aa1fd01975e3
euhoho <yosoydarky@gmail.com>
2026-05-05 11:35:37 +0200

    Part 2 exercise 8 - NumbersOfNumbers

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,19 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int ones = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number!= 0) {
+                ones = ones + 1;
+            }
 
+            if (number == 0) {
+                System.out.println("Number of numbers: " + ones);
+                break;
+            }
+        }
+        scanner.close();
     }
 }
