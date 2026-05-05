a0bbe7dad6738ee6eb49fac0e52e771878a4d02c
euhoho <yosoydarky@gmail.com>
2026-05-05 13:29:48 +0200

    Part 2 exercise 15 - CountingToHundred

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -5,6 +5,10 @@ public class CountingToHundred {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int number = Integer.parseInt(scanner.nextLine());
+        for (int i=number; i<101; i++) {
+            System.out.println(i);
+        }
+        scanner.close();
     }
 }
