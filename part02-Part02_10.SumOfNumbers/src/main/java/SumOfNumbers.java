de676d68e688340a235ac94c569e5264faf4fa08
euhoho <yosoydarky@gmail.com>
2026-05-05 11:48:18 +0200

    Part 2 exercise 10 - SumOfNumbers

--- part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -6,5 +6,18 @@ public class SumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+
+            if (number==0) {
+                System.out.println("Sum of the numbers: "+sum);
+                break;
+            }
+            sum = sum + number;
+        }
+        scanner.close();
     }
 }
