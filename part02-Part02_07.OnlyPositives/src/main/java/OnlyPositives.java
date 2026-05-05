5b711d376cf0aaf3da0373fdd8d38b893f1d83f5
euhoho <yosoydarky@gmail.com>
2026-05-05 11:19:10 +0200

    Part 2 exercise 7 - OnlyPositives

--- part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -6,5 +6,20 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+
+            if (number<0) {
+                System.out.println("Unsuitable number");
+                continue;
+            } else if (number==0) {
+                break;
+            } else if (number>0) {
+                int poweroftwo = number*number;
+                System.out.println(poweroftwo);
+            }
+        }
+        scanner.close();
     }
 }
