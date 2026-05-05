e422809691970721526d8625f4535a9b57798f68
euhoho <yosoydarky@gmail.com>
2026-05-05 10:58:54 +0200

    Part 2 exercise 4 - ComparingNUmbers

--- part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -6,5 +6,16 @@ public class ComparingNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int firstnumber = Integer.parseInt(scanner.nextLine());
+        int secondnumber = Integer.parseInt(scanner.nextLine());
+
+        if (firstnumber>secondnumber) {
+            System.out.println(firstnumber+" is greater than "+secondnumber);
+        } else if (firstnumber<secondnumber) {
+            System.out.println(firstnumber+" is smaller than "+secondnumber);
+        } else {
+            System.out.println(firstnumber+" is equal to "+secondnumber);
+        }
+        scanner.close();
     }
 }
