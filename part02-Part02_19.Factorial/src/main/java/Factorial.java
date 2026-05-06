c56ddc9dbe733222c9398ff0c116385280214ad0
euhoho <yosoydarky@gmail.com>
2026-05-06 14:07:00 +0200

    Part 2 exercise 19 - Factorial

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,16 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-    }
+        System.out.print("Give a number: ");
+        int number = Integer.parseInt(scanner.nextLine());
+
+        int mult = 1;
+
+        for(int i=1; number>=i; i++) {
+            System.out.println(i);
+            mult = mult * i;
+        }
+        System.out.println(mult);
+        scanner.close();
+    }   
 }
