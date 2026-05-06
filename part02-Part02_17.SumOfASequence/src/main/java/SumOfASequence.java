04b32a136ce3935022cf288ecf12742c5d795518
euhoho <yosoydarky@gmail.com>
2026-05-06 09:25:40 +0200

    Part 2 exercise 18 - SumOfAsequence

--- part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -5,6 +5,15 @@ public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Last number ? ");
+        int lastnumber = Integer.parseInt(scanner.nextLine());
+        int tulos = 0;
 
+        for(int i=1; i<=lastnumber; i++) {
+            tulos = i + tulos;
+        }
+        System.out.println(tulos);
+        scanner.close();
     }
 }
--- part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -6,5 +6,19 @@ public class SumOfASequenceTheSequel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("First number ? ");
+        int firstnumber = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Last number? ");
+        int lastnumber = Integer.parseInt(scanner.nextLine());
+
+        int tulos = 0;
+
+        for (int i=firstnumber; lastnumber>=i; i++) {
+            tulos = tulos + i;
+        }
+
+        System.out.println("The sum is "+ tulos);
+        scanner.close();
     }
 }
