3bac9776cb6787116224877c08e188ec028789e5
euhoho <yosoydarky@gmail.com>
2026-05-07 13:13:53 +0200

    Part 2 exercise 20 - RepeatingBreakingAndRemembering

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -7,34 +7,41 @@ public class RepeatingBreakingAndRemembering {
         Scanner scanner = new Scanner(System.in);
 
         int sum = 0;
-        int quantityOfNumbers = 0;
-        double average = 0.0;
+        int sumOfNumbers = 0;
+        double average = 0;
         int even = 0;
         int odd = 0;
 
-        System.out.println("Give numbers:");
+        System.out.println("Write numbers:");
 
-        while(true) {
+        while (true) {
             int number = Integer.parseInt(scanner.nextLine());
 
-            if (number==-1) {
+            if (number == -1) {
                 break;
             }
-            if (number%2==0) {
-                even++;
-            } else {
-                odd++;
-            }
+
             sum = sum + number;
-            quantityOfNumbers++;
-            average = (1.0*sum) / quantityOfNumbers;
+
+            sumOfNumbers = sumOfNumbers + 1;
+
+            average = (double) sum / sumOfNumbers;
+
+            if (number % 2 == 0) {
+                even = even +1;
+            }
+
+            if (number % 2 != 0) {
+                odd = odd +1;
+            }
         }
-        System.out.println("Thx! Bye!");
-        System.out.println("Sum: "+sum);
-        System.out.println("Numbers: "+quantityOfNumbers);
-        System.out.println("Average: "+average);
-        System.out.println("Even: "+even);
-        System.out.println("Odd: "+odd);
+
+        System.out.println("Thx, bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + sumOfNumbers);
+        System.out.println("Average: " + average);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
         scanner.close();
     }
 }
