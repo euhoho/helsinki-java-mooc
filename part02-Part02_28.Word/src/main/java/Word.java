186be86cadfcef5a6901875bc2a7d8dd44c5810c
euhoho <yosoydarky@gmail.com>
2026-05-08 11:29:17 +0200

    Part 2 exercise 28 - Word

--- part02-Part02_28.Word/src/main/java/Word.java
+++ part02-Part02_28.Word/src/main/java/Word.java
@@ -5,7 +5,13 @@ public class Word {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        word();
+        scanner.close();
+    }
 
+
+    public static String word() {
+        return "word";
     }
 
 }
