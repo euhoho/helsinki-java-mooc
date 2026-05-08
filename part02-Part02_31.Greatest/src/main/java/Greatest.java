beab31778b1a76a1727db2dffce9799656402b15
euhoho <yosoydarky@gmail.com>
2026-05-08 13:51:38 +0200

    Part 2 exercise 31 - Greatest

--- part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -1,10 +1,18 @@
-
 public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
-        //write some code here
 
-        return -1;
+        int greatest = number1;
+
+        if (number2 > greatest) {
+            greatest = number2;
+        }
+
+        if (number3 > greatest) {
+            greatest = number3;
+        }
+
+        return greatest;
     }
 
     public static void main(String[] args) {
