84f7b99e1a80209fd768e8c622b32e52393e3c06
euhoho <yosoydarky@gmail.com>
2026-05-08 11:16:25 +0200

    Part 2 exercise 26 -DIvisionByThree

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -5,7 +5,16 @@ public class DivisibleByThree {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        divisibleByThreeInRange(3, 6);
+        scanner.close();
+    }
 
+    public static void divisibleByThreeInRange( int beggining, int end) {
+        for(int i = beggining; i<=end; i++) {
+            if (i%3==0) {
+                System.out.println(i);
+            }
+        }
     }
 
 }
