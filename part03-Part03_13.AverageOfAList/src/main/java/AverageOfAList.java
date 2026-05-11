972d7f0de90e9407461153edbd029f76d62f7295
euhoho <yosoydarky@gmail.com>
2026-05-11 13:14:04 +0200

    Part 3 exercise 13 - AverageOfAList

--- part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
+++ part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -7,10 +7,25 @@ public class AverageOfAList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // implement here a program, that first reads user input
-        // adding them on a list until user gives -1.
-        // Then it computes the average of the numbers on the list
-        // and prints it.
-        
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(true) {
+            int input = Integer.parseInt(scanner.nextLine());
+            if (input==-1) {
+                break;
+            }
+            list.add(input);
+        }
+        int sum = 0;
+        int numbers = 0;
+        for( int number : list) {
+            sum = sum + number;
+            numbers++;
+        }
+
+        double average = (double) sum / numbers;
+
+        System.out.println("Average "+ average);
+
     }
 }
