2fc8682fc3e0d6521372798f1464120253bcc03d
euhoho <yosoydarky@gmail.com>
2026-05-11 11:55:10 +0200

    Part 3 exercise 9 - GreatestInList

--- part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
+++ part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
@@ -16,9 +16,18 @@ public class GreatestInList {
 
             list.add(input);
         }
-        
+
         System.out.println("");
 
-        // implement finding the greatest number in the list here
+        int smallest = list.get(0);
+
+        for (int i = 0; i < list.size(); i++) {
+            int number = list.get(i);
+            if (smallest < number) {
+                smallest = number;
+            }
+        }
+
+        System.out.println("The greatest number: " + smallest);
     }
 }
