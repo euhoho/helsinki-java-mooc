346567fd98da80257d2a177303230ed249dbe0f0
euhoho <yosoydarky@gmail.com>
2026-05-13 14:05:52 +0200

    Part 3 exercise 30 - AgeOfTheOldest

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,7 +5,23 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int oldest = 0;
+        while (true) {
+            String input = scanner.nextLine();
 
+            if (input.isEmpty()) {
+                break;
+            }
 
+            String [] parts = input.split(",");
+
+            int age = Integer.parseInt(parts[1]);
+
+            if (age>oldest) {
+                oldest = age;
+            }
+        }
+        System.out.println("Age of the oldest: "+oldest);
+        scanner.close();
     }
 }
