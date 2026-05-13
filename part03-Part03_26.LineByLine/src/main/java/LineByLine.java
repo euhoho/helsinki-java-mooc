f43fc739fa3a22a6aff3d12db367bf34b7b796a3
euhoho <yosoydarky@gmail.com>
2026-05-13 13:23:06 +0200

    Part 3 exercise 26 - LineByLine

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class LineByLine {
@@ -6,6 +5,20 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
 
+            if (input.isEmpty()) {
+                break;
+            }
+
+            String[] pieces = input.split(" ");
+
+            for (int i = 0; i < pieces.length; i++) {
+                System.out.println(pieces[i]);
+            }
+        }
+
+        scanner.close();
     }
 }
