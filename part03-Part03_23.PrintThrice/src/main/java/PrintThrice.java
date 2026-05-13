428940d6a6c32103602da0621fcad453934fcc96
euhoho <yosoydarky@gmail.com>
2026-05-13 13:02:07 +0200

    Part 3 exercise 23 - PrintThrice

--- part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
+++ part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -6,7 +6,10 @@ public class PrintThrice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.print("Give a world: ");
+        String word = scanner.nextLine();
+        System.out.print(word+word+word);
+        scanner.close();
 
     }
 }
