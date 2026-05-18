39d97da9511e7fad85f49778bcbd377849602703
euhoho <yosoydarky@gmail.com>
2026-05-18 09:40:11 +0200

    Part 4 exercise 2 - YourFirstBankTransfer

--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -2,7 +2,14 @@
 public class YourFirstBankTransfer {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // write your program here
+
+        Account number1 = new Account("Matthews account", 1000);
+        Account number2 = new Account("My account", 0);
+
+        number1.withdrawal(100);
+        number2.deposit(100.0);
+
+        System.out.println(number1);
+        System.out.println(number2);
     }
 }
